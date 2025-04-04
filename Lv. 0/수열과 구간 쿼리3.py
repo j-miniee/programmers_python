1fdc704134b0b592a6889402cb082b4c866bd079
j-miniee <potatopizza0518@naver.com>
2025-04-04 17:47:20 +0900

    25/4/4(금)

--- Lv. 0/수열과 구간 쿼리3.py
+++ Lv. 0/수열과 구간 쿼리3.py
@@ -1,21 +1,15 @@
 def solution(arr, queries):
     result = []
-    for query in queries:
-        s, e, k = query[0],query[1],query[2]
+    for s, e, k in queries:
         min_value = 1000000
         found = False
 
-        if s < k:
-            for i in range(k+1, e+1): #3, 4
+        for i in range(s, e+1):
+            if arr[i] > k:
                 if min_value > arr[i]:
                     min_value = arr[i]
                 found = True
 
-        elif s > k:
-            for i in range(s, e+1): #3
-                if min_value > arr[i]:
-                    min_value = arr[i]
-                found = True
         if found:
             result.append(min_value)
         else:
@@ -24,11 +18,12 @@ def solution(arr, queries):
     return result
 
 '''
-입력값 〉 [1, 2, 3, 4, 5, 6], [[2, 4, 5], [0, 3, 1], [2, 3, 2]]
-기댓값 〉 [-1, 2, 3]
+ s  e  k
+[2, 4, 5],  2<=i<=4, (i:2, 3, 4)   | arr[i] > 5
+[0, 3, 1],  0<=i<=3, (i:0, 1, 2, 3)| arr[i] > 1
+[2, 3, 2],  2<=i<=3, (i:2, 3)      | arr[i] > 2
 '''
 
-print(solution([0, 1, 2, 4, 3], [[0, 4, 2],[0, 3, 2],[0, 2, 2]]))
-# [3, 4, -1]
+print(solution( [1, 2, 3, 4, 5, 6], [[2, 4, 5], [0, 3, 1], [2, 3, 2]]))# [-1, 2, 3]
+print(solution([0, 1, 2, 4, 3], [[0, 4, 2],[0, 3, 2],[0, 2, 2]]))# [3, 4, -1]
 
-print(solution( [1, 2, 3, 4, 5, 6], [[2, 4, 5], [0, 3, 1], [2, 3, 2]]))
